b608eb789f6340cde3a9bf4981519714d42e5d3e
mjpsa2-iscte <78821675+mjpsa2-iscte@users.noreply.github.com>
2021-04-16 02:46:10 +0100

    A linkedList pode ser passada para String[][] com o dataFormater. Configurações - podem ser criados e importados ficheiros .xml para guardar configurações.

--- code_smell_detection/src/main/java/ES_2Sem_2021_Grupo_13/code_smell_detection/App.java
+++ code_smell_detection/src/main/java/ES_2Sem_2021_Grupo_13/code_smell_detection/App.java
@@ -36,6 +36,28 @@ import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
+import java.io.File;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import org.w3c.dom.Attr;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
+import java.io.File;
+
+
 /**
  * Hello world!
  *
@@ -54,9 +76,14 @@ public class App {
 	private static final String FILE_PATH = "C:\\Users\\maria\\Desktop\\App.java"; // file to analyse, debug
 	private static final String WRITEPATH = "C:\\Users\\maria\\Documents\\Code_Smells.xlsx";// will vary between
 																							// computers, code_smells.xlsx destination, debug
+	
+	private static final String XMLWRITEPATH = "C:\\Users\\maria\\Documents\\xml.xml";
+	
+	
 	private int countMethod = 1; //will count the number of methods, did not work inside the method writeOutClassMetrics 
 
 	private static LinkedList<String> realTest = new LinkedList<String>(); //will be used in writeOutClassMetrics to store the data to write to the .xlsx file
+	private static final int NUMBERPARAMETERS = 9;
 	private CompilationUnit compunit;
 	private List<MethodDeclaration> methods;
 	private List<ClassOrInterfaceDeclaration> classes;
@@ -133,8 +160,11 @@ public class App {
 	}
 
 	private static LinkedList<String> readFile() throws IOException { //reads the .xlsx file and puts its content on a linkedList
+		
+		//if you want a String [][] use the dataFormater(returnOfThisMethod) 
 
 		LinkedList<String> data = new LinkedList<String>();
+
 
 		String excelFilePath = WRITEPATH;
 		FileInputStream inputStream = new FileInputStream(new File(excelFilePath));
@@ -152,6 +182,8 @@ public class App {
 				if (cell.getCellType() != CellType.BLANK) { //if the cell is empty don't create a new position on the linkedList
 					System.out.print(cell.getStringCellValue()); //test
 					System.out.print(" - "); //test
+					
+					
 	
 					data.add(cell.getStringCellValue()); // adds the value, if not empty, to the linkedList
 				}
@@ -170,6 +202,10 @@ public class App {
 		return data; //returns linkedList
 
 	}
+	
+	
+	
+	
 	
 	private static void testeToLinkedList(LinkedList<String> data) { //prints all the values of the linkedList data, for debug
 		for(int i = 0; i < data.size(); i++)
@@ -221,19 +257,19 @@ public class App {
 
 	private static String[][] dataFormater(LinkedList<String> data) { // formats the data so it can be put in a .xlsx,
 																		// receives a LinkedList<String> and transforms into String [][]
-		int numberOfParameters = 9;
 
-		String[][] formatedData = new String[data.size() / numberOfParameters + numberOfParameters][numberOfParameters
+
+		String[][] formatedData = new String[data.size() / NUMBERPARAMETERS + NUMBERPARAMETERS][NUMBERPARAMETERS
 				+ 1]; // creates the array with the size required
 		String[] predefinido = { "MethodID", "package", "class", "method", "NOM_class", "LOC_class", "WMC_class",
 				"LOC_method", "CYCLO_method" }; // 1st line, titles
 
-		int nrLines = data.size() / numberOfParameters; // number of lines the final table will have (does not account
+		int nrLines = data.size() / NUMBERPARAMETERS; // number of lines the final table will have (does not account
 														// for the 1st)
 		int count = 0; // iterates the linkedList
 
 		for (int i = 1; i < nrLines + 2; i++) { // lines
-			for (int j = 1; j < numberOfParameters + 1; j++) { // columns
+			for (int j = 1; j < NUMBERPARAMETERS + 1; j++) { // columns
 
 				if (i == 1) {
 					formatedData[i - 1][j - 1] = predefinido[i * j - 1]; // writes the 1st line
@@ -245,8 +281,130 @@ public class App {
 		}
 		return formatedData; // returns the data, now as String [][]
 	}
+	
+	
+	private static void writeXMLFile(String NOM_Class_Parameters, String LOC_Class_Parameters, String WMC_class_Parameters, String is_God_Class_Parameters, String LOC_method_Parameters, String CYCLO_method_Parameters, String is_Long_Method_Parameters ) throws ParserConfigurationException, TransformerException { 
+		//writes parameters to a xml file
+		
+		DocumentBuilderFactory documentFactory = DocumentBuilderFactory.newInstance();
+		 
+        DocumentBuilder documentBuilder = documentFactory.newDocumentBuilder();
 
-	public static void main(String[] args) throws IOException {
+        Document document = documentBuilder.newDocument();
+
+        // root element
+        Element root = document.createElement("root");
+        document.appendChild(root);
+        
+        // configurations element
+        Element config  = document.createElement("configurations");
+        root.appendChild(config);
+        
+        
+        
+        // NOM_Class element
+        Element NOM_Class  = document.createElement("NOM_Class");
+        NOM_Class.appendChild(document.createTextNode(NOM_Class_Parameters));
+        config.appendChild(NOM_Class);
+        
+        // LOC_Class element
+        Element LOC_Class  = document.createElement("LOC_Class");
+        LOC_Class.appendChild(document.createTextNode(LOC_Class_Parameters));
+        config.appendChild(LOC_Class);
+        
+        // WMC_class element
+        Element WMC_class  = document.createElement("WMC_class");
+        WMC_class.appendChild(document.createTextNode(WMC_class_Parameters));
+        config.appendChild(WMC_class);
+        
+        // is_God_Class element
+        Element is_God_Class  = document.createElement("is_God_Class");
+        is_God_Class.appendChild(document.createTextNode(is_God_Class_Parameters));
+        config.appendChild(is_God_Class);
+        
+        // LOC_method element
+        Element LOC_method  = document.createElement("LOC_method");
+        LOC_method.appendChild(document.createTextNode(LOC_method_Parameters));
+        config.appendChild(LOC_method);
+        
+        // CYCLO_method element
+        Element CYCLO_method  = document.createElement("CYCLO_method");
+        CYCLO_method.appendChild(document.createTextNode(CYCLO_method_Parameters));
+        config.appendChild(CYCLO_method);
+        
+        // is_Long_Method element
+        Element is_Long_Method  = document.createElement("is_Long_Method");
+        is_Long_Method.appendChild(document.createTextNode(is_Long_Method_Parameters));
+        config.appendChild(is_Long_Method);
+        
+        // create the xml file
+        //transform the DOM Object to an XML File
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        Transformer transformer = transformerFactory.newTransformer();
+        DOMSource domSource = new DOMSource(document);
+        StreamResult streamResult = new StreamResult(new File(XMLWRITEPATH));
+
+		
+        transformer.transform(domSource, streamResult);
+        
+        System.out.println("Done creating XML File");
+
+		
+	}
+	
+	private static void readXMLFile(String path) {
+		
+        try {
+        	 
+            File xmlFile = new File(path);
+ 
+            DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance(); 
+            DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder(); 
+            Document doc = documentBuilder.parse(xmlFile); 
+            doc.getDocumentElement().normalize();
+ 
+            System.out.println("Root element :" + doc.getDocumentElement().getNodeName()); 
+            NodeList nodeList = doc.getElementsByTagName("configurations"); 
+            System.out.println("===============================================================");
+ 
+
+            for (int itr = 0; itr < nodeList.getLength(); itr++) {
+ 
+                Node node = nodeList.item(itr);
+ 
+                System.out.println("\nNode Name :" + node.getNodeName());
+ 
+                if (node.getNodeType() == Node.ELEMENT_NODE) {
+ 
+                    Element eElement = (Element) node;
+ 
+                    System.out.println("NOM_Class : "+ eElement.getElementsByTagName("NOM_Class").item(0).getTextContent());                    
+                    System.out.println("LOC_Class : "+ eElement.getElementsByTagName("LOC_Class").item(0).getTextContent());
+                    System.out.println("WMC_class : "+ eElement.getElementsByTagName("WMC_class").item(0).getTextContent());
+                    System.out.println("is_God_Class : "+ eElement.getElementsByTagName("is_God_Class").item(0).getTextContent());
+                    System.out.println("LOC_method : "+ eElement.getElementsByTagName("LOC_method").item(0).getTextContent());
+                    System.out.println("CYCLO_method : "+ eElement.getElementsByTagName("CYCLO_method").item(0).getTextContent());
+                    System.out.println("is_Long_Method : "+ eElement.getElementsByTagName("is_Long_Method").item(0).getTextContent());
+                    
+//        
+ 
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        //meter onde os dados??? LinkedList, array, outro??
+    }
+		
+
+	
+	
+	
+	
+	
+
+	public static void main(String[] args) throws IOException, ParserConfigurationException, TransformerException {
 
 		try {
 
@@ -267,6 +425,10 @@ public class App {
 
 			writeFile(WRITEPATH);
 			readFile();
+			
+			writeXMLFile("1", "2", "3", "4", "5", "6", "7"); 
+			
+			readXMLFile(XMLWRITEPATH);
 
 
 		} catch (FileNotFoundException e) {
@@ -275,4 +437,5 @@ public class App {
 		}
 
 	}
+
 }
