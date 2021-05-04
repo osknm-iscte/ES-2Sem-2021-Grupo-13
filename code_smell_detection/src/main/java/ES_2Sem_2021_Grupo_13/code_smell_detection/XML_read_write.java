6fd6874b74fab4f11d5db017ab992875b2bf9b7f
mjpsa2-iscte <78821675+mjpsa2-iscte@users.noreply.github.com>
2021-05-04 03:00:00 +0100

    XML agora permite escrever várias regras no mesmo ficheiro.

--- code_smell_detection/src/main/java/ES_2Sem_2021_Grupo_13/code_smell_detection/XML_read_write.java
+++ code_smell_detection/src/main/java/ES_2Sem_2021_Grupo_13/code_smell_detection/XML_read_write.java
@@ -1,8 +1,10 @@
 package ES_2Sem_2021_Grupo_13.code_smell_detection;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -10,6 +12,7 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
@@ -19,6 +22,7 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 import com.github.javaparser.JavaParser;
 import com.github.javaparser.ParserConfiguration;
@@ -31,20 +35,24 @@ public class XML_read_write {
 	
 	private final static String XMLWRITEPATH = "C:\\Users\\maria\\Documents\\xml_k.xml"; //has to be changed between computers	
 	
-	private static void writeXMLFile(String path, String ruleName, String NOM_Class_Parameters, String LOC_Class_Parameters, String WMC_class_Parameters, String is_God_Class_Parameters, String LOC_method_Parameters, String CYCLO_method_Parameters, String is_Long_Method_Parameters ) throws ParserConfigurationException, TransformerException { 
-		
-//	private static void writeXMLFile(LinkedList<String> valuesToWrite) throws ParserConfigurationException, TransformerException {
+	private static void writeXMLFile(String path, String ruleName, String NOM_Class_Parameters, String LOC_Class_Parameters, String WMC_class_Parameters, String is_God_Class_Parameters, String LOC_method_Parameters, String CYCLO_method_Parameters, String is_Long_Method_Parameters ) throws ParserConfigurationException, TransformerException, FileNotFoundException, SAXException, IOException { 
+
 		
 	//writes parameters to a xml file
 		
-//		App.writeOutClassMetrics("a", 1,1, methods);
+		File destination = new File (path);
 		
+		if(!destination.exists()) {
+		
+
 		DocumentBuilderFactory documentFactory = DocumentBuilderFactory.newInstance();
 		 
         DocumentBuilder documentBuilder = documentFactory.newDocumentBuilder();
 
         Document document = documentBuilder.newDocument();
 
+        
+        
         // root element
         Element root = document.createElement("root");
         document.appendChild(root);
@@ -58,9 +66,7 @@ public class XML_read_write {
 	     // nome da regra element
 	      Element nome_regra  = document.createElement("nome_regra");
 	      nome_regra.appendChild(document.createTextNode(ruleName));
-	      config.appendChild(nome_regra);
-      
-     
+	      config.appendChild(nome_regra);   
         
         
         // NOM_Class element
@@ -97,6 +103,7 @@ public class XML_read_write {
         Element is_Long_Method  = document.createElement("is_Long_Method");
         is_Long_Method.appendChild(document.createTextNode(is_Long_Method_Parameters));
         config.appendChild(is_Long_Method);
+         
         
         // create the xml file
         //transform the DOM Object to an XML File
@@ -104,9 +111,12 @@ public class XML_read_write {
         Transformer transformer = transformerFactory.newTransformer();
         DOMSource domSource = new DOMSource(document);
         StreamResult streamResult = new StreamResult(new File(path));
-
-		
+        
         transformer.transform(domSource, streamResult);
+		} else {
+			add_to_XML(path, ruleName, NOM_Class_Parameters, LOC_Class_Parameters, WMC_class_Parameters, is_God_Class_Parameters, LOC_method_Parameters, CYCLO_method_Parameters, is_Long_Method_Parameters);
+		}
+
         
         System.out.println("Done creating XML File");
 
@@ -199,30 +209,7 @@ public class XML_read_write {
 		
 	}
 	
-	
-		
-		
-	public static void main(String[] args) {
-		
-//		try {
-//			
-//			writeXMLFile(XMLWRITEPATH, "ruleName","1", "2", "3", "" , "5", "6", "7");		
-//		
-//		} catch (ParserConfigurationException | TransformerException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		} 	
-		
-//		formatText(data);
-		
-		
-		LinkedList<String> kant = readXMLFile(XMLWRITEPATH);
-		justRules(kant);
-
-
-	}
-
-	static void formatText(String path, String [] data) throws ParserConfigurationException, TransformerException {
+	static void formatText(String path, String [] data) throws ParserConfigurationException, TransformerException, FileNotFoundException, SAXException, IOException {
 		
 		String ruleName = "";
 		String nom_class = "";
@@ -279,6 +266,143 @@ public class XML_read_write {
 		writeXMLFile(path, ruleName, nom_class, loc_class, wmc_class, isgodclass_class, loc_method, cyclo_method, islong_method);
 		
 	}
+	
+	private static void add_to_XML (String path, String ruleName, String NOM_Class_Parameters, String LOC_Class_Parameters, String WMC_class_Parameters, String is_God_Class_Parameters, String LOC_method_Parameters, String CYCLO_method_Parameters, String is_Long_Method_Parameters ) throws ParserConfigurationException, FileNotFoundException, SAXException, IOException, TransformerException {
+		
+		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+//        dbf.setValidating(false);
+        DocumentBuilder db = dbf.newDocumentBuilder();
+         
+        Document document = db.parse(new FileInputStream(new File(path)));
+         
+        //xml content
+        
+        // find root
+        NodeList rootList = document.getElementsByTagName("root");
+        Node root = rootList.item(0);
+     
+        // configurations element
+        Element config  = document.getDocumentElement();
+        Node node_config = document.createElement("configurations");
+        root.appendChild(node_config);      
+        
+	     // nome da regra element
+
+//        Element nome_regra  = document.getDocumentElement();
+        Node node_nome_regra = document.createElement("nome_regra");
+	    node_nome_regra.appendChild(document.createTextNode(ruleName));
+        node_config.appendChild(node_nome_regra); 
+        
+      
+
+        
+        
+        // NOM_Class element
+        
+//        Element NOM_Class  = document.getDocumentElement();
+        Node node_NOM_Class = document.createElement("NOM_Class");
+        node_NOM_Class.appendChild(document.createTextNode(NOM_Class_Parameters));
+        node_config.appendChild(node_NOM_Class);
+        
+
+        
+        // LOC_Class element
+//        Element LOC_Class  = document.getDocumentElement();
+        Node node_LOC_Class = document.createElement("LOC_Class");
+        node_LOC_Class.appendChild(document.createTextNode(LOC_Class_Parameters));
+        node_config.appendChild(node_LOC_Class);
+        
+
+        
+        // WMC_class element
+        
+//        Element WMC_class  = document.getDocumentElement();
+        Node node_WMC_Class = document.createElement("WMC_class");
+        node_WMC_Class.appendChild(document.createTextNode(WMC_class_Parameters));
+        node_config.appendChild(node_WMC_Class);
+        
+        
+        
+        // is_God_Class element
+        
+//        Element is_God_Class  = document.getDocumentElement();
+        Node node_is_God_Class = document.createElement("is_God_Class");
+        node_is_God_Class.appendChild(document.createTextNode(is_God_Class_Parameters));
+        node_config.appendChild(node_is_God_Class);
+               
+        // LOC_method element
+        
+//        Element LOC_method  = document.getDocumentElement();
+        Node node_LOC_method = document.createElement("LOC_method");
+        node_LOC_method.appendChild(document.createTextNode(LOC_method_Parameters));
+        node_config.appendChild(node_LOC_method);
+        
+
+        
+        // CYCLO_method element
+        
+//        Element CYCLO_method  = document.getDocumentElement();
+        Node node_CYCLO_method = document.createElement("CYCLO_method");
+        node_CYCLO_method.appendChild(document.createTextNode(CYCLO_method_Parameters));
+        node_config.appendChild(node_CYCLO_method);
+
+        
+     // is_Long_Method element
+//        Element is_Long_Method  = document.getDocumentElement();
+        Node node_is_Long_Method = document.createElement("is_Long_Method");
+        node_is_Long_Method.appendChild(document.createTextNode(is_Long_Method_Parameters));
+        node_config.appendChild(node_is_Long_Method);
+        
+
+        
+      //transform the DOM Object to an XML File
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        Transformer transformer = transformerFactory.newTransformer();
+        DOMSource domSource = new DOMSource(document);
+        StreamResult streamResult = new StreamResult(new File(path));
+        
+        transformer.transform(domSource, streamResult);
+	}
+		
+	
+
+
+	public static void main(String[] args) {
+		
+			try {
+//				writeXMLFile(XMLWRITEPATH, "ruleName","1", "2", "3", "" , "5", "6", "7");	
+//				readXMLFile(XMLWRITEPATH);
+				writeXMLFile(XMLWRITEPATH, "rulename","a", "b", "c", "d" , "e", "f", "g");
+				readXMLFile(XMLWRITEPATH);
+			} catch (SAXException | IOException | ParserConfigurationException | TransformerException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		
+//		try {
+//			
+//			
+//			
+//			writeXMLFile(XMLWRITEPATH, "ruleName","1", "2", "3", "" , "5", "6", "7");	
+//			LinkedList<String> kant = readXMLFile(XMLWRITEPATH);
+//			writeXMLFile(XMLWRITEPATH, "ruleName","a", "b", "c", "d" , "e", "f", "g");
+//			
+//		
+//		} catch (ParserConfigurationException | TransformerException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		} 	
+		
+//		formatText(data);
+		
+		
+//		LinkedList<String> kant = readXMLFile(XMLWRITEPATH);
+//		justRules(kant);
+
+
+	}
+
+
 
 
 		
