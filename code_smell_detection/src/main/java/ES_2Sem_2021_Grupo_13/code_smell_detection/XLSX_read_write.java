64c40a9842ef01a679714eede75591f70317425b
mjpsa2-iscte <78821675+mjpsa2-iscte@users.noreply.github.com>
2021-05-08 15:25:36 +0100

    New branch to make JUnit tests.

--- code_smell_detection/src/main/java/ES_2Sem_2021_Grupo_13/code_smell_detection/XLSX_read_write.java
+++ code_smell_detection/src/main/java/ES_2Sem_2021_Grupo_13/code_smell_detection/XLSX_read_write.java
@@ -6,6 +6,7 @@ import java.io.FileNotFoundException;
 import java.util.Iterator;
 import java.util.List;
 import java.util.TreeSet;
+
 import com.github.javaparser.JavaParser;
 import com.github.javaparser.ParseResult;
 import com.github.javaparser.ParserConfiguration;
@@ -16,39 +17,50 @@ import com.github.javaparser.ast.comments.Comment;
 import com.github.javaparser.ast.comments.CommentsCollection;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
 import com.github.javaparser.printer.lexicalpreservation.LexicalPreservingPrinter;
+
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.LinkedList;
+
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.DataFormatter;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.TransformerException;
+
+
 
 /**
  * Hello world!
  *
  */
 public class XLSX_read_write {
+	
 
-	// computers,
-	// code_smells.xlsx
-	// destination, debug
 
-	private int countMethod = 1; // will count the number of methods, did not work inside the method
-									// writeOutClassMetrics
-	private static LinkedList<String> writedata = new LinkedList<String>(); // will be used in writeOutClassMetrics to
-	// store the data to write to the .xlsx file
-	private static final int NUMBERPARAMETERS = 11;
+
+	private static final String FILE_PATH = "C:\\Users\\maria\\Desktop\\App.java"; // file to analyse, debug
+	private static final String WRITEPATH = "C:\\Users\\maria\\Downloads\\Code_Smells (2).xlsx";// will vary between
+																							// computers, code_smells.xlsx destination, debug
+	
+	private static final String XMLWRITEPATH = "C:\\Users\\maria\\Documents\\xml.xml";
+	
+	
+	private int countMethod = 1; //will count the number of methods, did not work inside the method writeOutClassMetrics 
+
+	private static LinkedList<String> writedata = new LinkedList<String>(); //will be used in writeOutClassMetrics to store the data to write to the .xlsx file
+	private static final int NUMBERPARAMETERS = 9;
 	private CompilationUnit compunit;
 	private List<MethodDeclaration> methods;
 	private List<ClassOrInterfaceDeclaration> classes;
 
-	public XLSX_read_write(CompilationUnit compunit) { // ?
+	public XLSX_read_write(CompilationUnit compunit) { //?
 
 		this.compunit = compunit;
 		methods = compunit.findAll(MethodDeclaration.class);
@@ -56,27 +68,37 @@ public class XLSX_read_write {
 
 	}
 
+
+
+
+	public static void toTestreadFile(String path) throws IOException { 
 	
-private static LinkedList<String> readFile(String path) throws IOException { //reads the .xlsx file and puts its content on a linkedList
+		readFile(path);
+	
+	}
+
+
+
+	private static LinkedList<String> readFile(String path) throws IOException { //reads the .xlsx file and puts its content on a linkedList
 		
 		//if you want a String [][] use the dataFormater(returnOfThisMethod) 
 
-
 		LinkedList<String> data = new LinkedList<String>();
+
 
 //		String excelFilePath = WRITEPATH;
 		FileInputStream inputStream = new FileInputStream(new File(path));
 
 		Workbook workbook = new XSSFWorkbook(inputStream);
-		Sheet firstSheet = workbook.getSheetAt(0); // reads only the first sheet
-		Iterator<Row> iterator = firstSheet.iterator(); // creates an iterator to read the sheet
+		Sheet firstSheet = workbook.getSheetAt(0); //reads only the first sheet
+		Iterator<Row> iterator = firstSheet.iterator(); //creates an iterator to read the sheet
 
+		
 		while (iterator.hasNext()) {
-			Row nextRow = iterator.next(); // reads next row
-			Iterator<Cell> cellIterator = nextRow.cellIterator(); // will read the cells in each row
+			Row nextRow = iterator.next(); //reads next row
+			Iterator<Cell> cellIterator = nextRow.cellIterator(); //will read the cells in each row
 
 			while (cellIterator.hasNext()) {
-
 				Cell cell = cellIterator.next(); //goes to the next cell
 				if (cell.getCellType() != CellType.BLANK) { //if the cell is empty don't create a new position on the linkedList
 //					System.out.print(cell.getStringCellValue()); //test
@@ -100,7 +122,6 @@ private static LinkedList<String> readFile(String path) throws IOException { //r
 							
 						}
 						
-
 					}
 				}
 
@@ -108,32 +129,42 @@ private static LinkedList<String> readFile(String path) throws IOException { //r
 			System.out.println();
 		}
 
+		
 		workbook.close();
-		inputStream.close(); // closes reading
-
+		inputStream.close(); //closes reading
+		
 //		testeToLinkedList(data);	//debug
+		
 
-		return data; // returns linkedList
+		return data; //returns linkedList
+		
+		
 
 	}
-
+	
+	
+	
+	
+	
 //	private static void testeToLinkedList(LinkedList<String> data) { //prints all the values of the linkedList data, for debug
 //		for(int i = 0; i < data.size(); i++)
 //			
 //			System.out.println("\n" + data.get(i));
 //			
 //		}
+		
 
-	static void writeFile(String path, LinkedList<String> dataset) { // to use you can't have the file opened anywhere
-																		// else or else it will give errors on the
-																		// console
+
+
+
+	public static void writeFile(String path, LinkedList<String> dataset) { // to use you can't have the file opened anywhere else or else it will give errors on the console
 
 		XSSFWorkbook workbook = new XSSFWorkbook();
 		XSSFSheet sheet = workbook.createSheet("Code Smells"); // creates the .xlsx file
 
 		Object[][] datatypes = dataFormater(dataset); // gets the formated data
 
-		int rowNum = 0; // will count the rows
+		int rowNum = 0; //will count the rows
 		System.out.println("Creating excel");
 
 		for (Object[] datatype : datatypes) {
@@ -141,8 +172,7 @@ private static LinkedList<String> readFile(String path) throws IOException { //r
 			int colNum = 0;
 			for (Object field : datatype) {
 				Cell cell = row.createCell(colNum++);
-				if (field instanceof String) { // writes the string, i dont think the next condition will ever be used
-												// since the linkedList has Strings only
+				if (field instanceof String) { //writes the string, i dont think the next condition will ever be used since the linkedList has Strings only
 					cell.setCellValue((String) field);
 //				} else if (field instanceof Integer) {
 //					cell.setCellValue((Integer) field);
@@ -151,19 +181,18 @@ private static LinkedList<String> readFile(String path) throws IOException { //r
 		}
 
 		try {
-			FileOutputStream outputStream = new FileOutputStream(
-					System.getProperty("user.dir") + "/" + "Code_Smells.xlsx");
-			workbook.write(outputStream);// writes file
-			workbook.close();// closes file
+			FileOutputStream outputStream = new FileOutputStream(path);
+			workbook.write(outputStream);//writes file
+			workbook.close();//closes file
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 
-		System.out.println("Done"); // debug
+		System.out.println("Done"); //debug
 	}
-
+	
 	public static String[][] readyExcelForGUI(String excelFile) throws IOException {
 
 		return dataFormater(readFile(excelFile));
@@ -171,20 +200,13 @@ private static LinkedList<String> readFile(String path) throws IOException { //r
 	}
 
 	static String[][] dataFormater(LinkedList<String> data) { // formats the data so it can be put in a .xlsx,
-																// receives a LinkedList<String> and transforms into
-																// String [][]
+																		// receives a LinkedList<String> and transforms into String [][]
 
-		String[][] formatedData = new String[data.size() / NUMBERPARAMETERS + NUMBERPARAMETERS][NUMBERPARAMETERS + 1]; // creates
-																														// the
-																														// array
-																														// with
-																														// the
-																														// size
-																														// required
+
+		String[][] formatedData = new String[data.size() / NUMBERPARAMETERS + NUMBERPARAMETERS][NUMBERPARAMETERS
+				+ 1]; // creates the array with the size required
 		String[] predefinido = { "MethodID", "package", "class", "method", "NOM_class", "LOC_class", "WMC_class",
-				"is_God_Class",
-
-				"LOC_method", "CYCLO_method", "is_long_method" }; // 1st line, titles
+				"LOC_method", "CYCLO_method" }; // 1st line, titles
 
 		int nrLines = data.size() / NUMBERPARAMETERS; // number of lines the final table will have (does not account
 														// for the 1st)
@@ -203,61 +225,168 @@ private static LinkedList<String> readFile(String path) throws IOException { //r
 		}
 		return formatedData; // returns the data, now as String [][]
 	}
-
-	public static void confusionMatrixGC(LinkedList<String> data) {// TODO
-
-		int falso_positivo = 0;
-		int falso_negativo = 0;
-		int verdadeiro_positivo = 0;
-		int verdadeiro_negativo = 0;
-
-		// compare is_god_class with ??
-
-	}
-
-	public static LinkedList<Integer> confusionMatrixLM(LinkedList<String> data) {
-
-		int falso_positivo = 0;
-		int falso_negativo = 0;
-		int verdadeiro_positivo = 0;
-		int verdadeiro_negativo = 0;
-
-		int numComparacoes = 0;
-		// compare is_long_method with LOC_method
-		int count = 2;
-		int verif_condition = 0;
-		for (int i = 0; i < data.size(); i++) {
-
-			if (i == 7 * count) { // posicao do loc_method (?)
-
-//				if(data.get(i) == verif_condition && data.get(i+2) == True ) { //True && True
+	
+	
+	
+//	public static LinkedList<Integer> confusionMatrixGC (LinkedList<String> data) {//TODO
+//		
+//		int falso_positivo = 0;
+//		int falso_negativo = 0;
+//		int verdadeiro_positivo = 0;
+//		int verdadeiro_negativo = 0;
+//		
+//		//God Class code smell envolves WMC_Class and NOM_Class
+//
+//		int count = 2;
+//		
+//		int positionGodClassColumn = 9;
+//		
+//		int positionOf1stColumnRequired = 4; //NOM_Class position
+//		int nrColumnsUntil2ndColumnRequired = 2; //WMC_Class position
+//		
+//		
+//		for(int i = 0; i < data.size(); i++) {
+//			
+//			if ( i == positionOf1stColumnRequired*count ) { //posicao do loc_method (?)
+//								
+//				if(/* se data.get(i) verificar a condicao &&*/ Boolean.parseBoolean(data.get(i+positionGodClassColumn-positionOf1stColumnRequired)) == true ) { //True && True
 //					verdadeiro_positivo++;
 //				}
 //				
-//				if(data.get(i) == verif_condition && data.get(i+2) == True ) { //False && True
+//				if(/* se data.get(i) NAO verificar a condicao &&*/ Boolean.parseBoolean(data.get(i+positionGodClassColumn-positionOf1stColumnRequired)) == true ) { //False && True
 //					falso_negativo++;
 //				}
 //				
-//				if(data.get(i) == verif_condition && data.get(i+2) == False ) { //True && False
+//				if(/* se data.get(i) verificar a condicao &&*/ Boolean.parseBoolean(data.get(i+positionGodClassColumn-positionOf1stColumnRequired)) == false ) { //True && False
 //					falso_positivo++;
 //				}
 //				
-//				if(data.get(i) == verif_condition && data.get(i+2) == False ) { //False && False
+//				if(/* se data.get(i) NAO verificar a condicao &&*/ Boolean.parseBoolean(data.get(i+positionGodClassColumn-positionOf1stColumnRequired)) == false ) { //False && False
 //					verdadeiro_negativo++;
 //				}
+//				
+//				if(i % positionOf1stColumnRequired == 0) {
+//					count++;
+//				}
+//				
+//				
+//			}
+//			
+//		
+//		
+//		}
+//		
+//		LinkedList<Integer> resultados = new LinkedList<Integer>();
+//		int numComparacoes = verdadeiro_positivo + verdadeiro_negativo + falso_negativo + falso_positivo;
+//		resultados.add(verdadeiro_positivo);
+//		resultados.add(falso_negativo);
+//		resultados.add(falso_positivo);
+//		resultados.add(verdadeiro_negativo);
+//		resultados.add(numComparacoes);
+//		
+//		return resultados;	
+//		
+//		
+//		
+//		
+//	}
+//	
+//	
+//	public static LinkedList<Integer> confusionMatrixLM (LinkedList<String> data) { //TODO
+//		
+//		int falso_positivo = 0;
+//		int falso_negativo = 0;
+//		int verdadeiro_positivo = 0;
+//		int verdadeiro_negativo = 0;
+//		
+//		//Long Method code smell envolves LOC_Method and CYCLO_Method
+//		int count = 2;
+//		
+//		int positionLongMethodColumn = 10;
+//		
+//		int positionOf1stColumnRequired = 7; //LOC_Method position
+//		int nrColumnsUntil2ndColumnRequired = 1; //CYCLO_Method position
+//		
+//		
+//		for(int i = 0; i < data.size(); i++) {
+//			
+//			if ( i == positionOf1stColumnRequired*count ) { //posicao do loc_method (?)
+//								
+//				if(/* se data.get(i) verificar a condicao &&*/ Boolean.parseBoolean(data.get(i+positionLongMethodColumn-positionOf1stColumnRequired)) == true ) { //True && True
+//					verdadeiro_positivo++;
+//				}
+//				
+//				if(/* se data.get(i) NAO verificar a condicao &&*/ Boolean.parseBoolean(data.get(i+positionLongMethodColumn-positionOf1stColumnRequired)) == true ) { //False && True
+//					falso_negativo++;
+//				}
+//				
+//				if(/* se data.get(i) verificar a condicao &&*/ Boolean.parseBoolean(data.get(i+positionLongMethodColumn-positionOf1stColumnRequired)) == false ) { //True && False
+//					falso_positivo++;
+//				}
+//				
+//				if(/* se data.get(i) NAO verificar a condicao &&*/ Boolean.parseBoolean(data.get(i+positionLongMethodColumn-positionOf1stColumnRequired)) == false ) { //False && False
+//					verdadeiro_negativo++;
+//				}
+//				
+//				if(i % positionOf1stColumnRequired == 0) {
+//					count++;
+//				}
+//				
+//				
+//			}
+//			
+//		
+//		
+//		}
+//		
+//		LinkedList<Integer> resultados = new LinkedList<Integer>();
+//		int numComparacoes = verdadeiro_positivo + verdadeiro_negativo + falso_negativo + falso_positivo;
+//		resultados.add(verdadeiro_positivo);
+//		resultados.add(falso_negativo);
+//		resultados.add(falso_positivo);
+//		resultados.add(verdadeiro_negativo);
+//		resultados.add(numComparacoes);
+//		
+//		return resultados;		
+//		
+//	}
+	
 
-			}
+	
+	
+	
+	
+	
 
-		}
-		LinkedList<Integer> resultados = new LinkedList<Integer>();
-		numComparacoes = verdadeiro_positivo + verdadeiro_negativo + falso_negativo + falso_positivo;
-		resultados.add(verdadeiro_positivo);
-		resultados.add(falso_negativo);
-		resultados.add(falso_positivo);
-		resultados.add(verdadeiro_negativo);
-		resultados.add(numComparacoes);
-		return resultados;
-
-	}
+//public static void main(String[] args) throws IOException {
+//	
+//
+//
+//		try {
+//
+//			ParserConfiguration configuration = new ParserConfiguration();
+//			configuration.setLexicalPreservationEnabled(true);
+//			JavaParser javaParser = new JavaParser(configuration);
+//			CompilationUnit compunit = javaParser.parse(new File(FILE_PATH)).getResult().get();
+//
+//			List<MethodDeclaration> methods = compunit.findAll(MethodDeclaration.class);
+//
+//			for (MethodDeclaration m : methods) {
+//				System.out.println(m.getDeclarationAsString(true, true, true));
+//
+//			}
+//
+//			
+//
+////			writeFile(WRITEPATH, writedata);
+////			LinkedList <String> dataset = readFile(WRITEPATH);
+////			System.out.println("readfile  - " + readFile(WRITEPATH));
+////			writeFile("C:\\Users\\maria\\Desktop\\after_read.xlsx", dataset);
+//			} catch (FileNotFoundException e) {
+////			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//
+//	}
 
 }
---
+++ code_smell_detection/src/test/java/ES_2Sem_2021_Grupo_13/code_smell_detection/MethodComplexityInfoTest.java
@@ -0,0 +1,5 @@
+package ES_2Sem_2021_Grupo_13.code_smell_detection;
+
+public class MethodComplexityInfoTest {
+
+}
